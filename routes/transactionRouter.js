758c537790761a228b10c63404b928a4e62d7eb8
Cendikia <Halizah1612@gmail.com>
2022-07-11 23:38:37 +0700

    update userrouter

---
+++ routes/transactionRouter.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const transactionController = require("../app/controllers/api/v1/transactionController");
+const transactionMiddleware = require("../middlewares/transactionMiddleware");
+const userMiddleware = require("../middlewares/userMiddleware");
+
+router.get("/", userMiddleware.authorize, transactionController.list);
+
+router.get("/buyer/:id", userMiddleware.authorize, userMiddleware.isBuyyer, transactionController.listByBuyer);
+  
+router.get("/seller/:id", userMiddleware.authorize, userMiddleware.isSeller, transactionController.listBySeller);
+  
+router.get("/buyer/:userId/:id", userMiddleware.authorize, userMiddleware.isBuyyer, transactionController.showByBuyer);
+  
+router.get("/seller/:userId/:id", userMiddleware.authorize, userMiddleware.isSeller, transactionController.showBySeller);
+  
+router.post("/", userMiddleware.authorize, userMiddleware.isBuyyer, transactionMiddleware.getProductByUser, transactionController.create);
+  
+router.put("/:id", userMiddleware.authorize, userMiddleware.isSeller, transactionController.update)
+
+module.exports = router;
