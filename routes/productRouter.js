a05fca226ebea8b9681da60a966909dd168de902
Cendikia <Halizah1612@gmail.com>
2022-07-14 20:16:26 +0700

    update produk routes

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../app/controllers/api/v1/productController");
 const productMiddleware = require("../middlewares/productMiddleware");
+const memory = require("../config/memory");
 const userMiddleware = require("../middlewares/userMiddleware");
 
 router.get("/", productController.list);
@@ -12,10 +13,9 @@ router.get("/:id", productController.show);
 
 router.get("/seller/:id", userMiddleware.authorize, userMiddleware.isSeller, productController.showBySeller);
 
-router.post("/", userMiddleware.authorize, userMiddleware.isSeller, 
-productMiddleware.postValidate, productController.create);
+router.post("/", userMiddleware.authorize, userMiddleware.isSeller, memory.array("image", 2), productMiddleware.postValidate, productController.create);
 
-router.put("/:id", userMiddleware.authorize, userMiddleware.isSeller, productMiddleware.getById, productMiddleware.getBySellerId, productMiddleware.postValidate, productController.update);
+router.put("/:id", userMiddleware.authorize, userMiddleware.isSeller, memory.array("image",2), productMiddleware.getById, productMiddleware.getBySellerId, productMiddleware.postValidate, productController.update);
 
 router.delete("/:id", userMiddleware.authorize, userMiddleware.isSeller, productMiddleware.getById, productMiddleware.getBySellerId, productController.destroy);
 
